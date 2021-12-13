1e8ded687b850381d9c9c443a28f58fa7ac13415
clementlelievre <clement.lelievre90@gmail.com>
2021-12-13 18:17:51 +0100

    day 11 part 1 solved

--- 2021_11.py
+++ 2021_11.py
@@ -24,7 +24,8 @@ demo = """
 
 demo = [item for item in demo.split('\n') if item != '']
 demo = [[int(char) for char in item] for item in demo]
-
+input = [item for item in input.split('\n') if item != '']
+input = [[int(char) for char in item] for item in input]
 nb_flashes = 0
 
 def no_more_flash(cavern: list[list[int]]) -> bool:
@@ -33,46 +34,44 @@ def no_more_flash(cavern: list[list[int]]) -> bool:
             if octopus > 9:
                 return False
     return True
-
-def number_flashes(cavern: list[list[int]]) -> int:
-    return sum([len([octopus for octopus in row if octopus > 9]) for row in cavern])   
+   
    
 def flash(cavern: list[list[int]]) -> list[list[int]]:
     """updates the cavern values after flashes occur"""
     # increments by 1 octopuses adjacent to a flash (incl. diagonally adjacent)
     for r in range(len(cavern)):
-        for c in range(len(cavern[0])):
+        for c in range(len(cavern[0])):                
             if cavern[r][c] > 9:
                 try:
-                    cavern[r-1][c] += 1 if cavern[r-1][c] > 0 else 0
+                    cavern[r-1][c] += 1 if (cavern[r-1][c] >0 and r != 0) else 0
                 except IndexError:
                     pass
                 try:
-                    cavern[r+1][c] += 1 if cavern[r+1][c] > 0 else 0
+                    cavern[r+1][c] += 1 if cavern[r+1][c] >= 0 else 0
                 except IndexError:
                     pass
                 try:
-                    cavern[r-1][c-1] += 1 if cavern[r-1][c-1] > 0 else 0
+                    cavern[r-1][c-1] += 1 if ((cavern[r-1][c-1] >= 0) and (r!=0) and (c !=0)) else 0
                 except IndexError:
                     pass
                 try:
-                    cavern[r-1][c+1] += 1 if cavern[r-1][c+1] > 0 else 0
+                    cavern[r-1][c+1] += 1 if (cavern[r-1][c+1] >= 0 and r != 0) else 0
                 except IndexError:
                     pass
                 try:
-                    cavern[r+1][c+1] += 1 if cavern[r+1][c+1] > 0 else 0
+                    cavern[r+1][c+1] += 1 if cavern[r+1][c+1] >= 0 else 0
                 except IndexError:
                     pass
                 try:
-                    cavern[r+1][c-1] += 1 if cavern[r+1][c-1] > 0 else 0
+                    cavern[r+1][c-1] += 1 if (cavern[r+1][c-1] >= 0 and c != 0) else 0
                 except IndexError:
                     pass
                 try:
-                    cavern[r][c-1] += 1 if cavern[r][c-1] > 0 else 0
+                    cavern[r][c-1] += 1 if (cavern[r][c-1] >= 0 and c != 0) else 0
                 except IndexError:
                     pass
                 try:
-                    cavern[r][c+1] += 1 if cavern[r][c+1] > 0 else 0
+                    cavern[r][c+1] += 1 if cavern[r][c+1] >= 0 else 0
                 except IndexError:
                     pass
                 cavern[r][c] = -1
@@ -85,15 +84,20 @@ def step(cavern: list[list[int]]) -> list[list[int]]:
     for r in range(len(cavern)):
         for c in range(len(cavern[r])):
             cavern[r][c] += 1
-    nb_flashes += number_flashes(cavern)
+    #nb_flashes += number_flashes(cavern)
     while not no_more_flash(cavern):
         cavern = flash(cavern)
-        nb_flashes += number_flashes(cavern)
+        # for i in cavern:
+        #     print(*i)
+        # print('\n')
+        #nb_flashes += number_flashes(cavern)
+    nb_flashes += sum([len([octopus for octopus in row if octopus < 0]) for row in cavern])
     cavern = [[max(0, octopus) for octopus in row] for row in cavern]
     return cavern
 
-cavern = demo
+cavern = input
 for _ in range(100):
     cavern = step(cavern)
+    #print(cavern)
 print(nb_flashes)
     
