33423c7a8c81c74481d8b8f026adbbebe0e4eb6a
clementlelievre <clement.lelievre90@gmail.com>
2023-01-04 18:47:43 +0100

    graph traversal with 2 BFSs: with networkx, and coding my own BFS

--- training/2016_13.py
+++ training/2016_13.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import pandas as pd
 import networkx as nx
 from networkx.exception import (
@@ -72,13 +73,78 @@ while not solved:
 # part 2
 
 # How many locations (distinct x,y coordinates, including your starting location) can you reach in at most 50 steps?
-answer = len(
-    [v for v in nx.shortest_path_length(G=G, source=START_LOCATION).values() if v <= 50]
-)  # I found out in the docstring that not specifying a target square allows to call the function in all target nodes
-print(answer)
+SOLVE_PART2 = False
+if SOLVE_PART2:
+    answer = len(
+        [
+            v
+            for v in nx.shortest_path_length(G=G, source=START_LOCATION).values()
+            if v <= 50
+        ]
+    )  # I found out in the docstring that not specifying a target square allows to call the function in all target nodes
+    print("part 2:", answer)
 
 # I feel lucky because:
 # 1) the grid was nice in the sense that arbitrarily setting its size to 100*100 was enough to get both stars, without having to expand it
 # 2) leveraging the powerful networkx package yields potent results, and the heavy lifting is done for me
 
+#####################################################################################################################
 # Now that I've solved this challenge I feel like coding DFS/BFS by myself
+
+# redoing part 1 with BFS
+# I'll try to code the pseudo-code BFS given in the CLR (algo textbook by Cormen, Leiserson, Rivest and Stein)
+graph_dict = defaultdict(set)
+for row_ind in range(nb_rows):
+    for col_ind in range(nb_cols):
+        current_pos = (row_ind, col_ind)
+        current_type = grid[current_pos]
+        if current_type == ".":
+            if col_ind > 0 and grid[row_ind, col_ind - 1] == ".":
+                graph_dict[current_pos].add((row_ind, col_ind - 1))
+            if col_ind < nb_cols - 1 and grid[row_ind, col_ind + 1] == ".":
+                graph_dict[current_pos].add((row_ind, col_ind + 1))
+            if row_ind > 0 and grid[row_ind - 1, col_ind] == ".":
+                graph_dict[current_pos].add((row_ind - 1, col_ind))
+            if row_ind < nb_rows - 1 and grid[row_ind + 1, col_ind] == ".":
+                graph_dict[current_pos].add((row_ind + 1, col_ind))
+
+
+class Node:
+    """Represents a node of the graph"""
+
+    def __init__(self, coord: tuple[int, int], start_loc=START_LOCATION) -> None:
+        x, y = coord
+        self.x = x
+        self.y = y
+        self.color = "white" if (x, y) != start_loc else "gray"
+        self.distance = float("inf") if (x, y) != start_loc else 0
+        self.neighbours = graph_dict[coord]
+
+    def __repr__(self) -> str:
+        return f"Node({self.x}, {self.y}, {self.color}, {self.distance}, {self.parent})"
+
+    def __str__(self) -> str:
+        return f"Node({self.x}, {self.y}, {self.color}, {self.distance}, {self.parent})"
+
+
+G = {
+    Node(root): tuple(map(Node, nodes_list)) for root, nodes_list in graph_dict.items()
+}  # dict with nodes as keys and their neighbours as values
+queue = []
+queue.append(
+    [elem for elem in G if elem.x == 1 and elem.y == 1][0]
+)  # admittedly odd way to get the starting node
+while (
+    len(queue)
+    and [elem for elem in G if (elem.x, elem.y) == END_LOCATION][0].color != "black"
+):
+    current_node: Node = queue.pop(0)  # dequeue
+    for neighbour_coord in current_node.neighbours:
+        neighbour = [elem for elem in G if (elem.x, elem.y) == neighbour_coord][0]
+        if neighbour.color == "white":
+            neighbour.color = "gray"  # node is on the frontier
+            neighbour.distance = current_node.distance + 1
+            queue.append(neighbour)
+    current_node.color = "black"  # node is explored
+
+print([elem for elem in G if (elem.x, elem.y) == END_LOCATION][0].distance)
