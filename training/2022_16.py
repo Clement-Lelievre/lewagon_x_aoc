906f0becefb2f0e22bd6bee294a26b6119282430
clementlelievre <clement.lelievre90@gmail.com>
2023-01-12 17:37:18 +0100

    my solution works on all actual inputs I've seen but not on the example input :S

--- training/2022_16.py
+++ training/2022_16.py
@@ -1,6 +1,5 @@
 import re
-import networkx as nx
-from itertools import combinations_with_replacement
+from copy import deepcopy
 
 INPUT = """Valve QE has flow rate=3; tunnels lead to valves OU, ME, UX, AX, TW
 Valve TN has flow rate=16; tunnels lead to valves UW, CG, WB
@@ -74,61 +73,116 @@ Valve HH has flow rate=22; tunnel leads to valve GG
 Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II"""
 
-# note: from the text it appears that I can only look at paths that are 14 units long, not 15, because the 15th valve won't have time to work anyway
-# but the thing is, just because you enter a valve doesn't imply it's optimal to open it; it may only be on your way to a more
+
+# Notes:
+# in terms of graph theory, the graph is undirected and unweighted (unit weight everywhere)
+# a state is the combination of: time, position, and which valves opened
+# pruning the tree: I can safely discard a state if it's inferior to a visited state
+# how many states to visit? my input has 59 valves, but only 44 of them have flow rate = 0 so at best I just go through these rooms
+
+# from the text it appears that I can only look at paths that are 14 units long, not 15, because the 15th valve won't have time to work anyway
+# but the thing is, just because you enter a valve room doesn't imply it's optimal to open the valve; it may only be on your way to a more
 # rewarding valve so maybe the path will be longer than 14 units
 
-
-# extract valve info; a dict of dict looks suitable to store it
+# extract valve info; a dict of dicts looks suitable to store it
 valve_data_pattern = re.compile("^Valve (\w+).*?(\d+).*?valves? (.*?)$", re.IGNORECASE)
 valves = [row for v in INPUT.splitlines() if (row := v.strip())]
-
 valve_dict = {}
 for valve in valves:
     valve_name, valve_power, valve_connections = (
         (regex_search := valve_data_pattern.search(valve)).group(1),
         regex_search.group(2),
-        map(str.strip, regex_search.group(3).split(",")),
+        tuple(map(str.strip, regex_search.group(3).split(","))),
     )
     valve_dict[valve_name] = {
         "flow rate": int(valve_power),
         "connections": valve_connections,
     }
 
-# build the network
-G = nx.Graph()
-for start_valve in valve_dict:
-    for end_valve in valve_dict[start_valve].get("connections"):
-        G.add_edge(start_valve, end_valve)
-##########################################################
-# print(G)
-# valve_flows_sorted = sorted(
-#     [(v, flow) for v in valve_dict if (flow := valve_dict[v]["flow rate"])],
-#     reverse=True,
-#     key=lambda x: x[1],
-# )
-# print('Relevant valves sorted:\n', valve_flows_sorted, end="\n\n")
+# implement search algo
+valves_of_interest = {k: fr for k, v in valve_dict.items() if (fr := v["flow rate"])}
+max_pressure_released = 0
+visited = set()
+best_path = None
 
-# paths = nx.all_pairs_shortest_path(G, cutoff=14)
-# while True:
-#     path = next(paths)
-#     if path[0] == "AA":
-#         break
-    
-# paths_from_start = {k: v for k, v in path[1].items() if valve_dict[k].get("flow rate")}
-# paths_from_start = dict(zip())
-# print(paths_from_start)
 
-# print(list(nx.all_shortest_paths(G, "AA", "QN")))
+def explore_slow(
+    current_room: str,
+    time_left: int,
+    opened_valves: dict,
+    total_pressure_released: int,
+    current_path: list,
+) -> None:
+    """Slow version, because it also prints out the best path"""
+    global max_pressure_released, best_path
+    # assert time_left >= 0
+    if (current_state := (current_room, time_left, total_pressure_released)) in visited:
+        return
+    # if is_dominated(current_state):
+    #     return
+    visited.add(current_state)
+    current_path.append(current_state)
+    if time_left == 0 or len(opened_valves) == len(valves_of_interest):
+        if total_pressure_released > max_pressure_released:
+            best_path = current_path
+            max_pressure_released = total_pressure_released
+        return
+    if current_room in valves_of_interest and current_room not in opened_valves:
+        time_left -= 1
+        opened_valves[current_room] = time_left
+        total_pressure_released += time_left * valves_of_interest[current_room]
+        visited.add(current_state)
+        current_path.append((current_room, time_left, total_pressure_released))
+        if time_left == 0 or len(opened_valves) == len(valves_of_interest):
+            if total_pressure_released > max_pressure_released:
+                best_path = current_path
+                max_pressure_released = total_pressure_released
+            return
+    for next_room in valve_dict[current_room]["connections"]:
+        explore(
+            next_room,
+            time_left - 1,
+            deepcopy(opened_valves),
+            total_pressure_released,
+            deepcopy(current_path),
+        )
 
-def findPaths(G,u,n):
-    if n==0:
-        return [[u]]
-    paths = [[u]+path for neighbor in G.neighbors(u) for path in findPaths(G,neighbor,n-1) if u not in path]
-    return paths
 
-allpaths = []
-for node in G:
-    allpaths.extend(findPaths(G,node,18))
-    
-print(allpaths[0])
+def explore(
+    current_room: str,
+    time_left: int,
+    opened_valves: dict[str, int],
+    total_pressure_released: int,
+) -> None:
+    global max_pressure_released, best_path
+    # assert time_left >= 0
+    if (current_state := (current_room, time_left, total_pressure_released)) in visited:
+        return  # actually it could be that this isn't even enough to tell a state from a different one!
+    visited.add(current_state)
+    if time_left == 0 or len(opened_valves) == len(valves_of_interest):
+        if total_pressure_released > max_pressure_released:
+            max_pressure_released = total_pressure_released
+        return
+    if current_room in valves_of_interest and current_room not in opened_valves:
+        time_left -= 1
+        opened_valves[current_room] = time_left
+        total_pressure_released += time_left * valves_of_interest[current_room]
+        visited.add(current_state)
+        if time_left == 0 or len(opened_valves) == len(valves_of_interest):
+            if total_pressure_released > max_pressure_released:
+                max_pressure_released = total_pressure_released
+            return
+    for next_room in valve_dict[current_room]["connections"]:
+        explore(
+            next_room, time_left - 1, deepcopy(opened_valves), total_pressure_released
+        )
+
+
+explore("AA", 30, dict(), 0)
+print(max_pressure_released)  # 2077
+
+# my solution above works on every actual input I've come across (including mine of course), but not on the example input!
+# I think that's because I wrongly rule out some paths at the beginning of the function
+# so only half credit
+
+# part 2
