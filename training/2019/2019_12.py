b91bc7bbb7e71e74a9137b80d748117867ff4771
clementlelievre <clement.lelievre90@gmail.com>
2023-02-06 18:48:13 +0100

    solving part 2, detecting cycle length for each dimension

--- training/2019/2019_12.py
+++ training/2019/2019_12.py
@@ -11,10 +11,6 @@ TEST_INPUT = """<x=-8, y=-10, z=0>
 <x=2, y=-7, z=3>
 <x=9, y=-8, z=-3>"""
 
-TEST_INPUT = """<x=-1, y=0, z=2>
-<x=2, y=-10, z=-7>
-<x=4, y=-8, z=8>
-<x=3, y=5, z=-1>"""
 
 NB_PAT = re.compile(r"(-?\d+)")
 ROWS = INPUT.splitlines()
@@ -44,3 +40,71 @@ total_energy = sum(
     sum(map(abs, positions[k])) * sum(map(abs, velocities[k])) for k in positions
 )
 print(f"{total_energy=}")
+
+# part 2
+# let us find the cycle length for each planet on each of the three dimensions
+# we'll then find the least common multiple between those numbers
+ROWS = TEST_INPUT.splitlines()
+positions = {i: list(map(int, NB_PAT.findall(ROWS[i]))) for i in range(len(ROWS))}
+velocities = {i: [0, 0, 0] for i in positions}
+
+initial_x = [[positions[i][0], 0] for i in positions]
+initial_y = [[positions[i][1], 0] for i in positions]
+initial_z = [[positions[i][2], 0] for i in positions]
+
+# find cycle length for the x dimension
+c = 0
+while True:
+    # update gravity
+    for ind1, ind2 in combinations(velocities, 2):
+        if (pos1 := positions[ind1][0]) < (pos2 := positions[ind2][0]):
+            velocities[ind1][0] += 1
+            velocities[ind2][0] -= 1
+        elif pos1 > pos2:
+            velocities[ind1][0] -= 1
+            velocities[ind2][0] += 1
+    # update positions
+    for k in positions:
+        positions[k][0] += velocities[k][0]
+    if [[positions[i][0], 0] for i in positions] == initial_x:
+        break
+    c += 1
+print(c)
+
+# find cycle length for the y dimension
+c = 0
+while True:
+    # update gravity
+    for ind1, ind2 in combinations(velocities, 2):
+        if (pos1 := positions[ind1][1]) < (pos2 := positions[ind2][1]):
+            velocities[ind1][1] += 1
+            velocities[ind2][1] -= 1
+        elif pos1 > pos2:
+            velocities[ind1][1] -= 1
+            velocities[ind2][1] += 1
+    # update positions
+    for k in positions:
+        positions[k][1] += velocities[k][1]
+    if [[positions[i][1], 0] for i in positions] == initial_y:
+        break
+    c += 1
+print(c)
+
+# find cycle length for the z dimension
+c = 0
+while True:
+    # update gravity
+    for ind1, ind2 in combinations(velocities, 2):
+        if (pos1 := positions[ind1][2]) < (pos2 := positions[ind2][2]):
+            velocities[ind1][2] += 1
+            velocities[ind2][2] -= 1
+        elif pos1 > pos2:
+            velocities[ind1][2] -= 1
+            velocities[ind2][2] += 1
+    # update positions
+    for k in positions:
+        positions[k][2] += velocities[k][2]
+    if [[positions[i][2], 0] for i in positions] == initial_z:
+        break
+    c += 1
+print(c)
