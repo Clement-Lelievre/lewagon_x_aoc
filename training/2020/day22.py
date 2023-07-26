a422357f8a94abf7df8346183df4af4ce1252cbf
clementlelievre <clement.lelievre90@gmail.com>
2023-07-26 12:17:52 +0200

    solved day 22 both parts, with no optimization except using a deque for fast access (set/get) to both ends of an array

--- training/2020/day22.py
+++ training/2020/day22.py
@@ -1,5 +1,8 @@
 """https://adventofcode.com/2020/day/22"""
 
+from collections import deque
+
+
 TEST_INPUT = """Player 1:
 9
 2
@@ -69,7 +72,7 @@ Player 2:
 25
 """
 
-from collections import deque
+# I choose a deck not because it's about decks of cards, but for fast access to both ends of an array
 
 
 class CrabGame:
@@ -77,11 +80,12 @@ class CrabGame:
 
     def __init__(self, cards: str) -> None:
         self.cards = cards
+        self.make_decks()
 
-    def get_cards(self) -> None:
+    def make_decks(self) -> None:
         """Parses the input into two card decks"""
-        self.deck1 = deque([])
-        self.deck2 = deque([])
+        self.deck1: deque[int] = deque([])
+        self.deck2: deque[int] = deque([])
         current_deck = self.deck1
         for row in self.cards.splitlines()[1:]:
             if "Player" in row:
@@ -93,74 +97,75 @@ class CrabGame:
         ), "Both decks should initially have the same length"
         print("Both card decks were initialized")
 
-    def play_normal_game(self) -> None:
-        self.get_cards()
+    def play_normal_game(
+        self,
+    ) -> None:
+        """Playing the base version of the game"""
         while self.deck1 and self.deck2:
             a, b = self.deck1.popleft(), self.deck2.popleft()
-            if a > b:
-                self.deck1.extend([a, b])
-            else:
-                self.deck2.extend([b, a])
+            self.deck1.extend([a, b]) if a > b else self.deck2.extend([b, a])
         winner_deck = self.deck1 or self.deck2
         self.compute_winner_score(winner_deck)
 
-    def compute_winner_score(self, winner_deck: list[int]) -> None:
-        """Computes the winner's score as requested"""
+    def compute_winner_score(self, winner_deck: deque[int]) -> None:
+        """Computes the score as requested
+
+        Args:
+            winner_deck (deque[int]): the final deck of the winner of the game
+        """
         score = sum(
             val * (len(winner_deck) - ind) for ind, val in enumerate(winner_deck)
         )
         print(f"{score=}")
 
     def play_recursive_game(
-        self, top_game: bool = False, verbose: bool = False
-    ) -> tuple[int, list[int]]:
-        if top_game:  # top-level game
-            self.get_cards()
-            self.known_results = {}
+        self,
+        deck1: deque[int],
+        deck2: deque[int],
+    ) -> tuple[int, deque[int]]:
+        """Playing the recursive game
+
+        Args:
+            deck1 (deque[int]): _description_
+            deck2 (deque[int]): _description_
+
+        Returns:
+            tuple[int, deque[int]]: _description_
+        """
         seen = set()
-        while self.deck1 and self.deck2:
-            if (current_distrib := (tuple(self.deck1), tuple(self.deck2))) in seen:
-                if verbose:
-                    print("Cards already seen! Player 1 wins")
-                return 0, self.deck1
-            a, b = self.deck1.popleft(), self.deck2.popleft()
+        while deck1 and deck2:
+            if (current_distrib := (tuple(deck1), tuple(deck2))) in seen:
+                return 0, deck1  # player 1 wins
+            a, b = deck1.popleft(), deck2.popleft()
             seen.add(current_distrib)
-            if (len(self.deck1) >= a) and (len(self.deck2) >= b):
-                if current_distrib in self.known_results:
-                    round_winner_ind = self.known_results[current_distrib]
-                else:
-                    frozen_deck1, frozen_deck2 = deque(self.deck1), deque(self.deck2)
-                    if verbose:
-                        print("playing sub-game...")
-                    round_winner_ind, _ = self.play_recursive_game()
-                    self.known_results[current_distrib] = round_winner_ind
-                    self.deck1 = frozen_deck1  # reset initial deck, as it was modified by the above sub-game
-                    self.deck2 = frozen_deck2  # reset initial deck, as it was modified by the above sub-game
-                if (
-                    round_winner_ind == 0
-                ):  # player 1 won the sub-game, hence wins the current round
-                    self.deck1.extend((a, b))
-                else:
-                    self.deck2.extend((b, a))
-                continue
-            if a > b:
-                self.deck1.extend((a, b))
+            if len(deck1) >= a and len(deck2) >= b:
+                round_winner_ind, _ = self.play_recursive_game(
+                    deque(list(deck1)[:a]), deque(list(deck2)[:b])
+                )
+                deck2.extend((b, a)) if round_winner_ind else deck1.extend((a, b))
+            elif a > b:
+                deck1.extend((a, b))
             else:
-                self.deck2.extend((b, a))
-        winner_deck = self.deck1 or self.deck2
-        return (0 if winner_deck == self.deck1 else 1), winner_deck
+                deck2.extend((b, a))
+        winner_deck = deck1 or deck2
+        return (0 if winner_deck == deck1 else 1), winner_deck
 
 
 if __name__ == "__main__":
-    # part 1
+    # part 1 on test data
     test_game = CrabGame(TEST_INPUT)
     test_game.play_normal_game()
-    _, winner_deck = test_game.play_recursive_game(top_game=True)
-    test_game.compute_winner_score(winner_deck)
-
+    # part 2 on test data
+    test_game.make_decks()
+    _, winner_deck_ = test_game.play_recursive_game(
+        test_game.deck1,
+        test_game.deck2,
+    )
+    test_game.compute_winner_score(winner_deck_)
+    # part 1 on actual data
     real_game = CrabGame(INPUT)
     real_game.play_normal_game()
-    _, winner_deck = real_game.play_recursive_game(top_game=True)
-    real_game.compute_winner_score(winner_deck)
-
-    # part 2
+    # part 2 on actual data
+    real_game.make_decks()
+    _, winner_deck_ = real_game.play_recursive_game(real_game.deck1, real_game.deck2)
+    real_game.compute_winner_score(winner_deck_)
