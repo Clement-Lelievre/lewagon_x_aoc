8cd8523078dae1c6bfbaf98f8de7b4439d4e2c40
clementlelievre <clement.datascience@gmail.com>
2023-12-16 21:55:30 +0100

    ugly script should be refactored, but functional

--- training/2023/day13.py
+++ training/2023/day13.py
@@ -3,7 +3,8 @@
 Usage: python day13.py <input_filepath>
 """
 import argparse
-from tqdm import tqdm
+from typing import Generator
+
 import numpy as np
 
 TEST_DATA = """#.##..##.
@@ -39,7 +40,11 @@ def process_input(text: str) -> list[np.ndarray]:
     return grids
 
 
-def find_reflection(grid: np.ndarray) -> tuple[int]:
+def find_reflection(grid: np.ndarray) -> int:
+    """Find the uniqe reflection of a grid, if any, for part 1
+    specifically this returns the number of rows above the reflection
+    (or columns to the left if the reflection is vertical)
+    """
     for pat_width in range(grid.shape[0] // 2, 0, -1):
         for pat_top_line_ind in range(grid.shape[0] - 2 * pat_width + 1):
             if (
@@ -51,8 +56,27 @@ def find_reflection(grid: np.ndarray) -> tuple[int]:
                 pat_top_line_ind == 0
                 or pat_top_line_ind + pat_width * 2 == grid.shape[0]
             ):
-                return pat_width + pat_top_line_ind, pat_top_line_ind
-    return 0, None
+                return pat_width + pat_top_line_ind
+    return 0
+
+
+def find_all_reflections(grid: np.ndarray) -> Generator[int, None, None]:
+    """Find all the reflections of a grid, if any, for part 2
+    specifically this yields the number of rows above the reflection
+    (or columns to the left if the reflection is vertical)
+    """
+    for pat_width in range(grid.shape[0] // 2, 0, -1):
+        for pat_top_line_ind in range(grid.shape[0] - 2 * pat_width + 1):
+            if (
+                grid[pat_top_line_ind : pat_top_line_ind + pat_width]
+                == grid[
+                    pat_top_line_ind + pat_width : pat_top_line_ind + 2 * pat_width
+                ][::-1, :]
+            ).all() and (
+                pat_top_line_ind == 0
+                or pat_top_line_ind + pat_width * 2 == grid.shape[0]
+            ):
+                yield pat_width + pat_top_line_ind
 
 
 def main_p1(text: str) -> int:
@@ -60,8 +84,8 @@ def main_p1(text: str) -> int:
     nb_cols_left = 0
     nb_rows_above = 0
     for grid in grids:
-        nb_rows_above += (a := find_reflection(grid)[0])
-        nb_cols_left += (b := find_reflection(np.rot90(grid, k=3))[0])
+        nb_rows_above += (a := find_reflection(grid))
+        nb_cols_left += (b := find_reflection(np.rot90(grid, k=3)))
         assert a ^ b, "should be only one reflection"
         if nb_rows_above == 0 and nb_cols_left == 0:
             raise ValueError("no reflection found")
@@ -70,63 +94,50 @@ def main_p1(text: str) -> int:
     return answer
 
 
+def process_p2(grid: np.ndarray, line_ind_data: list, grid_ind: int) -> int:
+    for row_ind in range(grid.shape[0]):
+        for col_ind in range(grid.shape[1]):
+            fixed_grid = grid.copy()
+            fixed_grid[row_ind, col_ind] = "#" if grid[row_ind, col_ind] == "." else "."
+            for grid_nb_rows_above in find_all_reflections(fixed_grid):
+                if (
+                    grid_nb_rows_above != line_ind_data[grid_ind][1]
+                    or line_ind_data[grid_ind][0] == "v"
+                ):
+                    return grid_nb_rows_above * 100
+
+            for grid_nb_cols_above in find_all_reflections(np.rot90(fixed_grid, k=3)):
+                if grid_nb_cols_above != 0 and (
+                    grid_nb_cols_above != line_ind_data[grid_ind][1]
+                    or line_ind_data[grid_ind][0] == "h"
+                ):
+                    return grid_nb_cols_above
+
+
 def main_p2(text: str) -> int:
+    # TODO: clean this up. Could use a namedtuple for dot notation access and enhanced readability
     grids = process_input(text)
-    nb_cols_left = 0
-    nb_rows_above = 0
     line_ind_data = []
-    #find_reflection(grids[9]); import sys; sys.exit()
 
     for grid in grids:
-        if (line_ind_r := find_reflection(grid)[1]) is not None:
-            line_ind_data.append(("h", line_ind_r))
+        if p1_nb_rows_above := find_reflection(grid):
+            line_ind_data.append(("h", p1_nb_rows_above))
             continue
-        if (line_ind_c := find_reflection(np.rot90(grid, k=3))[1]) is not None:
-            line_ind_data.append(("v", line_ind_c))
+        if p1_nb_cols_left := find_reflection(np.rot90(grid, k=3)):
+            line_ind_data.append(("v", p1_nb_cols_left))
             continue
         raise ValueError("no reflection found")
     assert len(line_ind_data) == len(grids)
-
+    answer = 0
     for grid_ind, grid in enumerate(grids):
-        found = False
-        for row_ind in range(grid.shape[0]):
-            for col_ind in range(grid.shape[1]):
-                fixed_grid = grid.copy()
-                fixed_grid[row_ind, col_ind] = (
-                    "#" if grid[row_ind, col_ind] == "." else "."
-                )
-                nb_r_fixed, pat_top_line_ind = find_reflection(fixed_grid)
-                if nb_r_fixed > 0 and (
-                    pat_top_line_ind != line_ind_data[grid_ind][1]
-                    or line_ind_data[grid_ind][0] == "v"
-                ):
-                    nb_rows_above += nb_r_fixed
-                    found = True
-                    # print(f"{grid_ind=} {pat_top_line_ind=} {line_ind_data[grid_ind][1]=}")
-                    # print(f"found {nb_r_fixed} rows {row_ind=} {col_ind=}")
-                    #break
-                nb_c_fixed, pat_top_line_ind = find_reflection(np.rot90(fixed_grid, k=3))
-                if nb_c_fixed > 0 and (
-                    pat_top_line_ind != line_ind_data[grid_ind][1]
-                    or line_ind_data[grid_ind][0] == "h"
-                ):
-                    nb_cols_left += nb_c_fixed
-                    found = True
-                    #print(f"found {nb_c_fixed} cols {row_ind=} {col_ind=}")
-                    #break
-
-            if found:
-                break
-    # for row in grids[-1].T:
-    #     print(*row)
-    answer = nb_cols_left + 100 * nb_rows_above
+        answer += process_p2(grid, line_ind_data, grid_ind)
     print(f"part 2 {answer=}")
     return answer
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("input_path", help="Input filepath", type=str)
+    parser.add_argument("--input_path", help="Input filepath", type=str)
     args = parser.parse_args()
     input_path = args.input_path
     with open(input_path) as f:
